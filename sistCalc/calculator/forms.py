78dea9cd7172d3ae2387a99a9f3e87611b4c5029
Julian Honores <julian.honores@alumnos.ucn.cl>
2025-03-29 02:44:38 -0300

    hello

--- sistCalc/calculator/forms.py
+++ sistCalc/calculator/forms.py
@@ -4,11 +4,17 @@ class transformationForm(forms.Form):
 
     number = forms.DecimalField(label='Numero', max_digits=10, decimal_places=2)
 
+    num_to_convert = forms.ChoiceField(
+        label="Base de origen",
+        choices=[('decimal', 'Decimal'), ('binary', 'Binario'), ('octal', 'Octal'), ('hexadecimal', 'Hexadecimal')],
+        widget=forms.Select, required=False
+    )
+    
     conversion_type = forms.ChoiceField(
         label="Tipo de conversion",
-        choices=[('binary', 'A Binario'), ('octal', 'A Octal'), ('hexadecimal', 'A Hexadecimal')],
+        choices=[('decimal', 'Decimal'),('binary', 'A Binario'), ('octal', 'A Octal'), ('hexadecimal', 'A Hexadecimal')],
         widget=forms.Select, required=False
     )  
 
-    base = forms.IntegerField(label='Base')
+    #base = forms.IntegerField(label='Base')
     #result = forms.CharField(label='Result', max_length=100, required=False)   
--- sistCalc/calculator/views.py
+++ sistCalc/calculator/views.py
@@ -9,88 +9,84 @@ class index(View):
         form = transformationForm
         return render(request, 'calculator/index.html', {'form': form})
 
+    #def something_to_decimal(self, number, origin_base):
+        #if origin_base == 'binary':
+            
+        
+
+    def decimal_to_something(self, number, conversion_type):
+        lista = []
+        result = ''
+        if number % 1 == 0:
+            number = int(number)
+            
+            if conversion_type == "binary":
+                #loop
+                while number > 0:
+                    if number % 2 == 0:
+                        lista.append(0)
+                    else:
+                        lista.append(1)
+                    number = number//2
+    
+                lista.reverse()
+                result = lista
+            elif conversion_type == "octal":
+    
+                while number > 0:
+                    resto = number % 8  # Calcula el resto de la división entre 8
+                    lista.append(resto)  # Agrega el resto a la lista
+                    number = number // 8  # Divide el número entre 8 (división entera)
+                       
+                lista.reverse()
+                result = lista
+    
+            elif conversion_type == "hexadecimal":
+                while number > 0:
+                    division = number/16
+                    partEntera = math.trunc(division)
+                    partDecimal = division - partEntera
+                    resto = int(partDecimal*16)   
+                    if resto > 9:
+                        if resto == 10:
+                            resto = 'A'
+                        elif resto == 11:
+                            resto = 'B'
+                        elif resto == 12:
+                            resto = 'C'
+                        elif resto == 13:
+                            resto = 'D'
+                        elif resto == 14:
+                            resto = 'E'
+                        elif resto == 15:
+                            resto = 'F'
+                    lista.append(resto)
+                    number = partEntera
+                lista.reverse()
+                result = lista
+        return result        
+ 
     def post(self, request):
         form = transformationForm(request.POST)
 
         if form.is_valid():
             number = form.cleaned_data['number']
-            base = form.cleaned_data['base']
+            #base = form.cleaned_data['base']
             conversion_type = form.cleaned_data['conversion_type']
-            lista = []
-            result = ''
+            origin_base = form.cleaned_data['num_to_convert']	
 
+            if conversion_type == origin_base:
+                return render(request, 'calculator/index.html', {'form': form, 'result': 'Error: No se puede convertir a la misma base.'})
 
-            if number % 1 == 0:
-                number = int(number)
+            if origin_base == 'decimal':
+                result = self.decimal_to_something(number, conversion_type)
+            elif origin_base == 'binary':
+                #primero debemos pasar de binario a decimal
+                result = self.something_to_decimal(number, origin_base)
                 
-                if conversion_type == "binary":
-                    #loop
-                    while number > 0:
-                        if number % 2 == 0:
-                            lista.append(0)
-                        else:
-                            lista.append(1)
-                        number = number//2
-
-                    lista.reverse()
-                    result = lista
-                elif conversion_type == "octal":
-
-                    while number > 0:
-                        if number % 8 == 0:
-                            lista.append(0)
-                        else:
-                            division = number/8
-                            division = round(division, 1)
-                            pEntera = math.trunc(division)
-                            divStr = str(division)
-                            pDecimal = divStr[-1]
-                            pDecimal = int(pDecimal)
-
-                        number = pEntera
-                        lista.append(pDecimal)    
-
-
-
-                    lista.reverse()
-                    result = lista
-
-
-                elif conversion_type == "hexadecimal":
-                    while number > 0:
-                        division = number/16
-                        partEntera = math.trunc(division)
-                        partDecimal = division - partEntera
-                        resto = int(partDecimal*16)   
-                        if resto > 9:
-                            if resto == 10:
-                                resto = 'A'
-                            elif resto == 11:
-                                resto = 'B'
-                            elif resto == 12:
-                                resto = 'C'
-                            elif resto == 13:
-                                resto = 'D'
-                            elif resto == 14:
-                                resto = 'E'
-                            elif resto == 15:
-                                resto = 'F'
-                        lista.append(resto)
-                        number = partEntera
-                    lista.reverse()
-                    result = lista
-
-
-            else:
-                if base == 2:
-                    result = format(float(number), '.2f')
-                elif base == 8:
-                    result = format(float(number), '.2f')
-                elif base == 16:
-                    result = format(float(number), '.2f')
-                else:
-                    result = str(number)
+            result = ''.join(map(str, result))
 
             return render(request, 'calculator/index.html', {'form': form, 'result': result})
+    
         else:
             return render(request, 'calculator/index.html', {'form': form})
