8729ff255073f0755e3a0dfa995e034d51ccda7a
markzuckerberg44 <julian.honores@alumnos.ucn.cl>
2025-03-30 18:45:52 -0300

    agregue nueva funcion que pasa de algo a decimal pero tiene un bug con los primitivos

--- sistCalc/calculator/views.py
+++ sistCalc/calculator/views.py
@@ -9,9 +9,51 @@ class index(View):
         form = transformationForm
         return render(request, 'calculator/index.html', {'form': form})
 
-    #def something_to_decimal(self, number, origin_base):
-        #if origin_base == 'binary':
+    def something_to_decimal(self, number, origin_base):
+
+        #logica por si recibimos un numero entero a transformar
+        if number%1 == 0:
+            n = len(str(number))-1
+            i = 0
+            digitos = [int(d) for d in str(number)]
+            resultado = ""
+            if origin_base == 'binary':
+                while i <= n:
+                    operacion = digitos[i] * (2**(n-i))
+                    resultado+= str(operacion)
+                    i+=1
+            elif origin_base == 'octal':
+                while i <= n:
+                    operacion = digitos[i] * (8**(n-i))
+                    resultado+= str(operacion)
+                    i+=1
+            elif origin_base == 'hexadecimal':
+                
+                while i <= n:
+                    if type(digitos[i]) == str:
+                        if digitos[i] == 'A':
+                            digitos[i] = 10
+                        elif digitos[i] == 'B':
+                            digitos[i] = 11
+                        elif digitos[i] == 'C':
+                            digitos[i] = 12
+                        elif digitos[i] == 'D':
+                            digitos[i] = 13
+                        elif digitos[i] == 'E':
+                            digitos[i] = 14
+                        elif digitos[i] == 'F':
+                            digitos[i] = 15
+                    operacion = digitos[i] * (16**(n-i))
+                    resultado+= str(operacion)
+                    i+=1
+
+            resultado = int(resultado)
+            # aqui implementamos la logica si recibimos un numero con "," para transformar
             
+
+        return resultado
+
+                
         
 
     def decimal_to_something(self, number, conversion_type):
@@ -80,10 +122,15 @@ class index(View):
 
             if origin_base == 'decimal':
                 result = self.decimal_to_something(number, conversion_type)
+
             elif origin_base == 'binary':
                 #primero debemos pasar de binario a decimal
                 result = self.something_to_decimal(number, origin_base)
-                
+                #despues de decimal a la base que queremos
+                if conversion_type != 'decimal':
+                    result = self.decimal_to_something(result, conversion_type)
+            
+            result = list(result)
             result = ''.join(map(str, result))
 
             return render(request, 'calculator/index.html', {'form': form, 'result': result})
